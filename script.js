3c8df68e793a2667553b8bb86042ace7de32f7f4
khushboo <khushbookhushboo20093@gmail.com>
2025-01-15 08:59:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let student = {
+	name: "John Doe"
+	object.prototype.getkeys = function(){
+	return object.keys(this);
+	};
+    console.log(student.getkeys())
+};
